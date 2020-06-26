bf8f76b2dc70da361b60ee06b5ddb38d2ec7b37e
Jerry <85411418@qq.com>
2020-06-27 01:18:24 +0800

    finish lru cache

---
+++ lru/cache_test.go
@@ -0,0 +1,30 @@
+package lru
+
+import (
+	"testing"
+
+	"github.com/iGoogle-ink/gotil/xlog"
+)
+
+func TestNewCache(t *testing.T) {
+	cache := NewCache(2)
+
+	cache.Put("1", "one")
+	xlog.Info(cache.Get("1"))
+
+	xlog.Warning("===============")
+	cache.Put("2", "two")
+	xlog.Info(cache.Get("1"))
+
+	xlog.Warning("===============")
+	cache.Put("3", "three")
+	xlog.Info(cache.Get("2"))
+	xlog.Info(cache.Get("3"))
+	xlog.Info(cache.Get("3"))
+	xlog.Info(cache.Get("1"))
+
+	xlog.Warning("===============")
+	cache.Put("2", "two")
+	xlog.Info(cache.Get("3"))
+	xlog.Info(cache.Get("1"))
+}
