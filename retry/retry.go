6379afdf599437a5b3a58f4422e06cb90c59851c
Jerry <85411418@qq.com>
2020-07-31 10:50:41 +0800

    add retry func

--- retry/retry.go
+++ retry/retry.go
@@ -6,7 +6,7 @@ import (
 	"github.com/iGoogle-ink/gotil/xlog"
 )
 
-// Retry retries the callback func if some error is raised.
+// Retry 重试 func 最大次数，间隔
 func Retry(callback func() error, maxRetries int, interval time.Duration) error {
 	var err error
 	for i := 1; i <= maxRetries; i++ {
