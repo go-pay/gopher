e971ad1ff0afa5fcdb392dd6537f0452558b9ab4
Jerry <85411418@qq.com>
2021-06-04 18:46:50 +0800

    优化 BodyMap

--- bm/body_map.go
+++ bm/body_map.go
@@ -14,17 +14,51 @@ import (
 
 type BodyMap map[string]interface{}
 
-var mu sync.RWMutex
+type xmlMapMarshal struct {
+	XMLName xml.Name
+	Value   interface{} `xml:",cdata"`
+}
+
+type xmlMapUnmarshal struct {
+	XMLName xml.Name
+	Value   string `xml:",cdata"`
+}
+
+type File struct {
+	Name    string `json:"name"`
+	Content []byte `json:"content"`
+}
+
+var mu = new(sync.RWMutex)
 
 // 设置参数
-func (bm BodyMap) Set(key string, value interface{}) {
+func (bm BodyMap) Set(key string, value interface{}) BodyMap {
 	mu.Lock()
 	bm[key] = value
 	mu.Unlock()
+	return bm
 }
 
-// 获取参数
-func (bm BodyMap) Get(key string) string {
+func (bm BodyMap) SetBodyMap(key string, value func(bm BodyMap)) BodyMap {
+	_bm := make(BodyMap)
+	value(_bm)
+
+	mu.Lock()
+	bm[key] = _bm
+	mu.Unlock()
+	return bm
+}
+
+// 设置 FormFile
+func (bm BodyMap) SetFormFile(key string, file *File) BodyMap {
+	mu.Lock()
+	bm[key] = file
+	mu.Unlock()
+	return bm
+}
+
+// 获取参数转换string
+func (bm BodyMap) GetString(key string) string {
 	if bm == nil {
 		return util.NULL
 	}
@@ -41,19 +75,14 @@ func (bm BodyMap) Get(key string) string {
 	return v
 }
 
-func convertToString(v interface{}) (str string) {
-	if v == nil {
-		return util.NULL
+// 获取原始参数
+func (bm BodyMap) GetInterface(key string) interface{} {
+	if bm == nil {
+		return nil
 	}
-	var (
-		bs  []byte
-		err error
-	)
-	if bs, err = json.Marshal(v); err != nil {
-		return util.NULL
-	}
-	str = string(bs)
-	return
+	mu.RLock()
+	defer mu.RUnlock()
+	return bm[key]
 }
 
 // 删除参数
@@ -63,14 +92,24 @@ func (bm BodyMap) Remove(key string) {
 	mu.Unlock()
 }
 
-type xmlMapMarshal struct {
-	XMLName xml.Name
-	Value   interface{} `xml:",cdata"`
+// 置空BodyMap
+func (bm BodyMap) Reset() {
+	mu.Lock()
+	for k := range bm {
+		delete(bm, k)
+	}
+	mu.Unlock()
 }
 
-type xmlMapUnmarshal struct {
-	XMLName xml.Name
-	Value   string `xml:",cdata"`
+func (bm BodyMap) JsonBody() (jb string) {
+	mu.Lock()
+	defer mu.Unlock()
+	bs, err := json.Marshal(bm)
+	if err != nil {
+		return ""
+	}
+	jb = string(bs)
+	return jb
 }
 
 func (bm BodyMap) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
@@ -82,7 +121,7 @@ func (bm BodyMap) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error)
 		return
 	}
 	for k := range bm {
-		if v := bm.Get(k); v != util.NULL {
+		if v := bm.GetString(k); v != util.NULL {
 			e.Encode(xmlMapMarshal{XMLName: xml.Name{Local: k}, Value: v})
 		}
 	}
@@ -121,7 +160,7 @@ func (bm BodyMap) EncodeWeChatSignParams(apiKey string) string {
 	sort.Strings(keyList)
 	mu.RUnlock()
 	for _, k := range keyList {
-		if v := bm.Get(k); v != util.NULL {
+		if v := bm.GetString(k); v != util.NULL {
 			buf.WriteString(k)
 			buf.WriteByte('=')
 			buf.WriteString(v)
@@ -147,7 +186,7 @@ func (bm BodyMap) EncodeAliPaySignParams() string {
 	sort.Strings(keyList)
 	mu.RUnlock()
 	for _, k := range keyList {
-		if v := bm.Get(k); v != util.NULL {
+		if v := bm.GetString(k); v != util.NULL {
 			buf.WriteString(k)
 			buf.WriteByte('=')
 			buf.WriteString(v)
@@ -160,12 +199,13 @@ func (bm BodyMap) EncodeAliPaySignParams() string {
 	return buf.String()[:buf.Len()-1]
 }
 
+// ("bar=baz&foo=quux")
 func (bm BodyMap) EncodeGetParams() string {
 	var (
 		buf strings.Builder
 	)
 	for k, _ := range bm {
-		if v := bm.Get(k); v != util.NULL {
+		if v := bm.GetString(k); v != util.NULL {
 			buf.WriteString(k)
 			buf.WriteByte('=')
 			buf.WriteString(v)
@@ -181,7 +221,7 @@ func (bm BodyMap) EncodeGetParams() string {
 func (bm BodyMap) CheckEmptyError(keys ...string) error {
 	var emptyKeys []string
 	for _, k := range keys {
-		if v := bm.Get(k); v == util.NULL {
+		if v := bm.GetString(k); v == util.NULL {
 			emptyKeys = append(emptyKeys, k)
 		}
 	}
@@ -189,4 +229,19 @@ func (bm BodyMap) CheckEmptyError(keys ...string) error {
 		return errors.New(strings.Join(emptyKeys, ", ") + " : cannot be empty")
 	}
 	return nil
+}
+
+func convertToString(v interface{}) (str string) {
+	if v == nil {
+		return util.NULL
+	}
+	var (
+		bs  []byte
+		err error
+	)
+	if bs, err = json.Marshal(v); err != nil {
+		return util.NULL
+	}
+	str = string(bs)
+	return
 }
