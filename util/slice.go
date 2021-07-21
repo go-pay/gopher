da5a61f053c0b2f8142393c751c9fc20d5bddb2e
Jerry <85411418@qq.com>
2021-07-21 15:15:22 +0800

    update

--- util/slice.go
+++ util/slice.go
@@ -14,10 +14,41 @@ func MergeSliceRemoveDuplicate(slice1, slice2 []int) (merged []int) {
 	return merged
 }
 
-// 过滤数组 去除src中item，在dst中存在的item
-// src[1,2,3,4,5]   dst[2,4,6,8]	result[1,3,5]
+// Deprecated
 func FilterSlice(src []int, dst []int) (result []int) {
 	aMap := make(map[int]struct{})
+	for _, v := range dst {
+		aMap[v] = struct{}{}
+	}
+	for _, v := range src {
+		if _, has := aMap[v]; !has {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+// 过滤数组，去除src中item，在dst中存在的item
+// src[1,2,3,4,5]   dst[2,4,6,8]	result[1,3,5]
+func FilterIntSlice(src []int, dst []int) (result []int) {
+	aMap := make(map[int]struct{})
+	result = make([]int, 0)
+	for _, v := range dst {
+		aMap[v] = struct{}{}
+	}
+	for _, v := range src {
+		if _, has := aMap[v]; !has {
+			result = append(result, v)
+		}
+	}
+	return result
+}
+
+// 过滤数组，去除src中item，在dst中存在的item
+// src["a","b","c","d","e"]   dst["b","d","f","h"]	result["a","c","e"]
+func FilterStringSlice(src []string, dst []string) (result []string) {
+	aMap := make(map[string]struct{})
+	result = make([]string, 0)
 	for _, v := range dst {
 		aMap[v] = struct{}{}
 	}
