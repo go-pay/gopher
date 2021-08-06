3937ff30824dca43f8318adc34273e20cedc4638
Jerry <85411418@qq.com>
2021-08-06 13:41:20 +0800

    fix

--- des/des_cbc_decrypt.go
+++ des/des_cbc_decrypt.go
@@ -7,8 +7,18 @@ import (
 )
 
 // 解密数据的Bytes数组
+func CBCTripleDecryptData(secretData, key []byte) ([]byte, error) {
+	return tripleDecrypt(secretData, key)
+}
+
+// 解密数据的Bytes数组
 func CBCDecryptData(secretData, key []byte) ([]byte, error) {
 	return decrypt(secretData, key)
+}
+
+// 解密数据的Bytes数组
+func CBCTripleDecryptIvData(secretData, key, iv []byte) ([]byte, error) {
+	return tripleDecryptIv(secretData, key, iv)
 }
 
 // 解密数据的Bytes数组
@@ -16,7 +26,7 @@ func CBCDecryptIvData(secretData, key, iv []byte) ([]byte, error) {
 	return decryptIv(secretData, key, iv)
 }
 
-func decrypt(secretData, key []byte) (originByte []byte, err error) {
+func tripleDecrypt(secretData, key []byte) (originByte []byte, err error) {
 	block, err := des.NewTripleDESCipher(key)
 	if err != nil {
 		return nil, err
@@ -31,8 +41,38 @@ func decrypt(secretData, key []byte) (originByte []byte, err error) {
 	return PKCS7UnPadding(originByte), nil
 }
 
-func decryptIv(secretData, desKey, iv []byte) (originByte []byte, err error) {
+func decrypt(secretData, key []byte) (originByte []byte, err error) {
+	block, err := des.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	originByte = make([]byte, len(secretData))
+	blockMode.CryptBlocks(originByte, secretData)
+	if len(originByte) == 0 {
+		return nil, errors.New("blockMode.CryptBlocks error")
+	}
+	return PKCS7UnPadding(originByte), nil
+}
+
+func tripleDecryptIv(secretData, desKey, iv []byte) (originByte []byte, err error) {
 	block, err := des.NewTripleDESCipher(desKey)
+	if err != nil {
+		return nil, err
+	}
+	blockMode := cipher.NewCBCDecrypter(block, iv[:block.BlockSize()])
+
+	originByte = make([]byte, len(secretData))
+	blockMode.CryptBlocks(originByte, secretData)
+	if len(originByte) == 0 {
+		return nil, errors.New("blockMode.CryptBlocks error")
+	}
+	return PKCS7UnPadding(originByte), nil
+}
+
+func decryptIv(secretData, desKey, iv []byte) (originByte []byte, err error) {
+	block, err := des.NewCipher(desKey)
 	if err != nil {
 		return nil, err
 	}
--- des/des_test.go
+++ des/des_test.go
@@ -7,6 +7,9 @@ import (
 )
 
 var (
+	secretKey1 = "hehehaha"
+	iv1        = "12378945"
+
 	secretKey = "GYBh3Rmey7nNzR/NpV0vAw=="
 	iv        = "JR3unO2glQuMhUx3"
 )
@@ -14,13 +17,13 @@ var (
 func TestDesCBCEncryptDecrypt(t *testing.T) {
 	originData := "www.gopay.ink"
 	xlog.Debug("originData:", originData)
-	encryptData, err := CBCEncryptData([]byte(originData), []byte(secretKey))
+	encryptData, err := CBCEncryptData([]byte(originData), []byte(secretKey1))
 	if err != nil {
 		xlog.Error("DesCBCEncryptData:", err)
 		return
 	}
 	xlog.Debug("encryptData:", string(encryptData))
-	origin, err := CBCDecryptData(encryptData, []byte(secretKey))
+	origin, err := CBCDecryptData(encryptData, []byte(secretKey1))
 	if err != nil {
 		xlog.Error("DesCBCDecryptData:", err)
 		return
@@ -31,13 +34,47 @@ func TestDesCBCEncryptDecrypt(t *testing.T) {
 func TestDesCBCEncryptDecryptIv(t *testing.T) {
 	originData := "www.gopay.ink"
 	xlog.Debug("originData:", originData)
-	encryptData, err := CBCEncryptIvData([]byte(originData), []byte(secretKey), []byte(iv))
+	encryptData, err := CBCEncryptIvData([]byte(originData), []byte(secretKey1), []byte(iv1))
 	if err != nil {
 		xlog.Error("DesCBCEncryptIvData:", err)
 		return
 	}
 	xlog.Debug("encryptData:", string(encryptData))
-	origin, err := CBCDecryptIvData(encryptData, []byte(secretKey), []byte(iv))
+	origin, err := CBCDecryptIvData(encryptData, []byte(secretKey1), []byte(iv1))
+	if err != nil {
+		xlog.Error("DesCBCDecryptIvData:", err)
+		return
+	}
+	xlog.Debug("origin:", string(origin))
+}
+
+func TestDesCBCTripleEncryptDecrypt(t *testing.T) {
+	originData := "www.gopay.ink"
+	xlog.Debug("originData:", originData)
+	encryptData, err := CBCTripleEncryptData([]byte(originData), []byte(secretKey))
+	if err != nil {
+		xlog.Error("DesCBCEncryptData:", err)
+		return
+	}
+	xlog.Debug("encryptData:", string(encryptData))
+	origin, err := CBCTripleDecryptData(encryptData, []byte(secretKey))
+	if err != nil {
+		xlog.Error("DesCBCDecryptData:", err)
+		return
+	}
+	xlog.Debug("origin:", string(origin))
+}
+
+func TestDesCBCTripleEncryptDecryptIv(t *testing.T) {
+	originData := "www.gopay.ink"
+	xlog.Debug("originData:", originData)
+	encryptData, err := CBCTripleEncryptIvData([]byte(originData), []byte(secretKey), []byte(iv))
+	if err != nil {
+		xlog.Error("DesCBCEncryptIvData:", err)
+		return
+	}
+	xlog.Debug("encryptData:", string(encryptData))
+	origin, err := CBCTripleDecryptIvData(encryptData, []byte(secretKey), []byte(iv))
 	if err != nil {
 		xlog.Error("DesCBCDecryptIvData:", err)
 		return
