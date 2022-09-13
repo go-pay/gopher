1c2b54fcc42b056dfebace7b10657276cfc3871f
Jerry <85411418@qq.com>
2022-09-13 17:08:09 +0800

    add iterator

---
+++ iterator/step.go
@@ -0,0 +1,25 @@
+package iterator
+
+type Step struct {
+	Head int
+	Tail int
+}
+
+// Steps calculates the steps.
+// for _, step := range pkg.Steps(len(ids), 10) {
+// 		cur := ids[step.Head:step.Tail]
+//	}
+func Steps(total, step int) (steps []Step) {
+	steps = make([]Step, 0)
+	for i := 0; i < total; i++ {
+		if i%step == 0 {
+			head := i
+			tail := head + step
+			if tail > total {
+				tail = total
+			}
+			steps = append(steps, Step{Head: head, Tail: tail})
+		}
+	}
+	return steps
+}
