a5481a21fa9d1695913c01acb3f7218df3cdf08a
Jerry <85411418@qq.com>
2023-02-23 13:25:05 +0800

    update mod version go-redis up to v9

--- go.mod
+++ go.mod
@@ -6,44 +6,47 @@ require (
 	github.com/BurntSushi/toml v1.2.1
 	github.com/apache/rocketmq-client-go/v2 v2.1.1
 	github.com/eclipse/paho.mqtt.golang v1.4.2
-	github.com/gin-gonic/gin v1.8.1
-	github.com/go-redis/redis/v8 v8.11.5
+	github.com/gin-gonic/gin v1.9.0
 	github.com/google/uuid v1.3.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/opentracing/opentracing-go v1.2.0
+	github.com/redis/go-redis/v9 v9.0.2
 	github.com/uber/jaeger-client-go v2.30.0+incompatible
 	go.uber.org/zap v1.24.0
-	google.golang.org/genproto v0.0.0-20221207170731-23e4bf6bdc37
-	google.golang.org/grpc v1.51.0
+	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f
+	google.golang.org/grpc v1.53.0
 	google.golang.org/protobuf v1.28.1
 	gopkg.in/yaml.v3 v3.0.1
-	gorm.io/driver/mysql v1.4.4
-	gorm.io/gorm v1.24.2
+	gorm.io/driver/mysql v1.4.7
+	gorm.io/gorm v1.24.5
 )
 
 require (
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/bytedance/sonic v1.8.0 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.0 // indirect
-	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.10.0 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.2 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/goccy/go-json v0.10.0 // indirect
 	github.com/golang/mock v1.3.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
-	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/satori/go.uuid v1.2.0 // indirect
 	github.com/sirupsen/logrus v1.4.0 // indirect
@@ -51,16 +54,17 @@ require (
 	github.com/tidwall/gjson v1.13.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
-	github.com/ugorji/go/codec v1.2.7 // indirect
+	github.com/ugorji/go/codec v1.2.9 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/net v0.0.0-20221014081412-f15817d10f9b // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
-	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
-	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/term v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- orm/redis.go
+++ orm/redis.go
@@ -7,64 +7,43 @@ import (
 	"time"
 
 	"github.com/go-pay/gopher/xtime"
-	"github.com/go-redis/redis/v8"
+	"github.com/redis/go-redis/v9"
 )
 
 // RedisConfig redis config.
 type RedisConfig struct {
-	Addrs        []string       `json:"addrs" yaml:"addrs" toml:"addrs"`
-	Password     string         `json:"password" yaml:"password" toml:"password"`
-	DB           int            `json:"db" yaml:"db" toml:"db"`
-	ReadTimeout  xtime.Duration `json:"read_timeout" yaml:"read_timeout" toml:"read_timeout"`
-	WriteTimeout xtime.Duration `json:"write_timeout" yaml:"write_timeout" toml:"write_timeout"`
-	TLSCfg       *tls.Config    `json:"-" yaml:"-" toml:"-"`
-}
-
-type RedisCluster struct {
-	Redis *redis.ClusterClient
-}
-
-func InitRedisCluster(c *RedisConfig) (rc *RedisCluster) {
-	opts := &redis.ClusterOptions{
-		Addrs:    c.Addrs,
-		Password: c.Password,
-	}
-	if c.TLSCfg != nil {
-		opts.TLSConfig = c.TLSCfg
-	}
-	if c.ReadTimeout != 0 {
-		opts.ReadTimeout = time.Duration(c.ReadTimeout)
-	}
-	if c.WriteTimeout != 0 {
-		opts.WriteTimeout = time.Duration(c.WriteTimeout)
-	}
-	rd := redis.NewClusterClient(opts)
-	_, err := rd.Ping(context.Background()).Result()
-	if err != nil {
-		panic(fmt.Sprintf("failed to connect redis error:%+v", err))
-	}
-	rc = &RedisCluster{Redis: rd}
-	return rc
+	Addrs        []string                                        `json:"addrs" yaml:"addrs" toml:"addrs"`
+	Username     string                                          `json:"username" yaml:"username" toml:"username"`
+	Password     string                                          `json:"password" yaml:"password" toml:"password"`
+	DB           int                                             `json:"db" yaml:"db" toml:"db"`
+	ReadTimeout  xtime.Duration                                  `json:"read_timeout" yaml:"read_timeout" toml:"read_timeout"`
+	WriteTimeout xtime.Duration                                  `json:"write_timeout" yaml:"write_timeout" toml:"write_timeout"`
+	PoolSize     int                                             `json:"pool_size" yaml:"pool_size" toml:"pool_size"`
+	MaxIdleConn  int                                             `json:"max_idle_conn" yaml:"max_idle_conn" toml:"max_idle_conn"`
+	TLSCfg       *tls.Config                                     `json:"-" yaml:"-" toml:"-"`
+	Limiter      redis.Limiter                                   `json:"-" yaml:"-" toml:"-"`
+	OnConnFunc   func(ctx context.Context, cn *redis.Conn) error `json:"-" yaml:"-" toml:"-"`
 }
 
 type Redis struct {
 	Redis *redis.Client
 }
 
+type RedisCluster struct {
+	Redis *redis.ClusterClient
+}
+
 func InitRedis(c *RedisConfig) (r *Redis) {
 	opts := &redis.Options{
-		Addr:     c.Addrs[0],
-		Password: c.Password,
-		DB:       c.DB,
-	}
-	if c.TLSCfg != nil {
-		opts.TLSConfig = c.TLSCfg
-	}
-	if c.ReadTimeout != 0 {
-		opts.ReadTimeout = time.Duration(c.ReadTimeout)
-	}
-	if c.WriteTimeout != 0 {
-		opts.WriteTimeout = time.Duration(c.WriteTimeout)
+		Addr:         c.Addrs[0],
+		OnConnect:    c.OnConnFunc,
+		Username:     c.Username,
+		Password:     c.Password,
+		DB:           c.DB,
+		PoolSize:     c.PoolSize,
+		ReadTimeout:  time.Duration(c.ReadTimeout),
+		WriteTimeout: time.Duration(c.WriteTimeout),
+		TLSConfig:    c.TLSCfg,
 	}
 	rd := redis.NewClient(opts)
 	_, err := rd.Ping(context.Background()).Result()
@@ -75,26 +54,46 @@ func InitRedis(c *RedisConfig) (r *Redis) {
 	return r
 }
 
-func (r *Redis) Transaction(ctx context.Context, fc func(tx redis.Pipeliner) error) error {
-	txPip := r.Redis.TxPipeline()
-	defer txPip.Close()
-	err := fc(txPip)
-	if err != nil {
-		txPip.Discard()
-		return err
+func InitRedisCluster(c *RedisConfig) (rc *RedisCluster) {
+	opts := &redis.ClusterOptions{
+		Addrs:        c.Addrs,
+		OnConnect:    c.OnConnFunc,
+		Username:     c.Username,
+		Password:     c.Password,
+		PoolSize:     c.PoolSize,
+		ReadTimeout:  time.Duration(c.ReadTimeout),
+		WriteTimeout: time.Duration(c.WriteTimeout),
+		TLSConfig:    c.TLSCfg,
 	}
-	txPip.Exec(ctx)
-	return nil
+	rd := redis.NewClusterClient(opts)
+	_, err := rd.Ping(context.Background()).Result()
+	if err != nil {
+		panic(fmt.Sprintf("failed to connect redis error:%+v", err))
+	}
+	rc = &RedisCluster{Redis: rd}
+	return rc
+}
+
+func (r *Redis) Transaction(ctx context.Context, fc func(tx redis.Pipeliner) error) error {
+	_, err := r.Redis.TxPipelined(ctx, fc)
+	//txPip := r.Redis.TxPipeline()
+	//err := fc(txPip)
+	//if err != nil {
+	//	txPip.Discard()
+	//	return err
+	//}
+	//_, err = txPip.Exec(ctx)
+	return err
 }
 
 func (r *RedisCluster) Transaction(ctx context.Context, fc func(tx redis.Pipeliner) error) error {
-	txPip := r.Redis.TxPipeline()
-	defer txPip.Close()
-	err := fc(txPip)
-	if err != nil {
-		txPip.Discard()
-		return err
-	}
-	txPip.Exec(ctx)
-	return nil
+	_, err := r.Redis.TxPipelined(ctx, fc)
+	//txPip := r.Redis.TxPipeline()
+	//err := fc(txPip)
+	//if err != nil {
+	//	txPip.Discard()
+	//	return err
+	//}
+	//_, err = txPip.Exec(ctx)
+	return err
 }
